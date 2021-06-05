791ab8a37a4148effdfac0c72a18416b2d0e1961
Vinicius Almeida Silva <vinicius_almeidasilva@outlook.com>
2021-06-05 07:42:43 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+    </div>
+  );
+}
+
+export default App;
