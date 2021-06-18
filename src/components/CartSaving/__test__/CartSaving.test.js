80dbd13674aa9a91191a83b1581988fa965c17aa
Vinicius Almeida Silva <vinicius_almeidasilva@outlook.com>
2021-06-17 22:48:29 -0300

    fix: adjust tests

--- src/components/CartSaving/__test__/CartSaving.test.js
+++ src/components/CartSaving/__test__/CartSaving.test.js
@@ -11,11 +11,11 @@ describe('<CartSaving />', () => {
       let wrapper = shallow(<CartSaving />);
       expect(wrapper.contains('span')).toBe(false);
     });
-
   })
+
   describe(('When cart is not empty'), () => {
 
-    it('return number of product when there is products in the cart', () => {
+    it('return a component of product when there are products in the cart', () => {
       let wrapper = shallow(<CartSaving quatityOfProducts={10} />);
       should(wrapper.find('span')).to.have.lengthOf(1);
     });
@@ -24,34 +24,25 @@ describe('<CartSaving />', () => {
       const location = window.location;
       delete window.location;
       window.location = {
-          ...location,
-          reload: jest.fn()
-      };
+        ...location,
+        reload: jest.fn()
+      };  
   
-  
-      const myMock = jest.fn();
-  
-  
-      window.location.reload = jest.fn();
-  
+      const goToCartMock = jest.fn();  
   
       const props = {
         history: {
-          push: myMock
+          push: goToCartMock
         }
       }
       
       let wrapper = shallow(<CartSaving quatityOfProducts={10} {...props}  />);
       wrapper.find('span').simulate('click');
-      expect(myMock).toHaveBeenCalledTimes(1);
+      expect(goToCartMock).toHaveBeenCalledTimes(1);
+      expect(goToCartMock).toHaveBeenLastCalledWith('/cart');
       expect(window.location.reload).toHaveBeenCalledTimes(1);
       jest.restoreAllMocks();
       window.location = location;
     });
   })
-
-
-
-
-
 });
